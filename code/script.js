d646dce5af310eacb772f85efd43c25fa61039d1
Christoph Butenschön <c.butenschoen@gmail.com>
2019-07-29 21:13:03 +0200

    moved a lot of the background work (where is the player in the map, does he want to move to a wall tile?) from the mapLevel[][] board onto the objects, because that architecture was doing more harm than helping; `start game` now also creates a monster at a fixed location, which moves one tile in a random direction when the player moves

--- code/script.js
+++ code/script.js
@@ -1,23 +1,29 @@
 let gameCanvas
 let ctx
 let playerCharacter
+let monster1
 // mapSize = how many tiles the map square has
-let mapSize = 20;
+let mapSize = 10;
+let tileSize = 25;
 let levelMap = []
+let monstersInLevel = []
 
 class cell {
     constructor(x, y) {
         this.x = x;
         this.y = y;
-        this.heigth = 50;
-        this.width = 50;
-        this.border = true;
-        this.isPlayerInCell = false;
+        this.heigth = tileSize;
+        this.width = tileSize;
+        this.wall = true;
     }
 }
 
 class player {
     constructor(x, y) {
+        this.maximumHealth = 100;
+        this.currentHealth = 100;
+        this.strength = 30;
+        this.armour = 8;
         this.x = x;
         this.y = y;
     }
@@ -27,28 +33,43 @@ class player {
     }
 }
 
-function populateLevelMapWithCells() {
+class monster {
+    constructor(x, y) {
+        this.maximumHealth = 25;
+        this.currentHealth = 25;
+        this.strength = 10;
+        this.x = x;
+        this.y = y;
+    }
+    moveDirection(horizontal, vertical) {
+        this.x += horizontal;
+        this.y += vertical;
+    }
+}
+
+function populateLevelMapWithCells(level) {
     for (i = 0; i < mapSize; i++) {
         levelMap[i] = new Array()
         for (j = 0; j < mapSize; j++) {
-            cellObject = new cell((i * 50), (j * 50))
+            cellObject = new cell((i * tileSize), (j * tileSize))
             levelMap[i].push(cellObject)
         }
     }
-    levelMap[1][1].border = false
-    levelMap[1][1].isPlayerInCell = true
-    levelMap[1][2].border = false
-    levelMap[1][3].border = false
-    levelMap[2][3].border = false
-    levelMap[2][4].border = false
-    levelMap[3][4].border = false
 }
+function createMonsters(howMany) {
+    for (i = 0; i < howMany; i++) {
+        let monsterVariable = new monster(6 * tileSize, 7 * tileSize)
+        monstersInLevel.push(monsterVariable)
+    }
+}
+
+
 
 function drawCells() {
     for (i = 0; i < levelMap.length; i++) {
         for (j = 0; j < levelMap.length; j++) {
             let location = levelMap[i][j]
-            if (location.border == true) {
+            if (location.wall == true) {
                 ctx.fillStyle = "DarkGrey"
                 ctx.fillRect(location.x, location.y, location.heigth, location.width)
                 ctx.fillStyle = "black"
@@ -62,26 +83,65 @@ function drawCells() {
     }
 }
 
-function findPlayer() {
-    for (i = 0; i < levelMap.length; i++) {
-        for (j = 0; j < levelMap.length; j++) {
-            if (levelMap[i][j].isPlayerInCell == true) {
-                return [i, j]
-            }
-        }
-    }
-}
-
 function clearBoard() {
     fillStyle = "black"
     ctx.fillRect(0, 0, 500, 500)
 }
 
 function drawPlayer() {
-    player = new Image(50, 50)
-    player.src = './images/big_zombie_idle_anim_f0.png';
-    player.onload = function () {
-        ctx.drawImage(player, (findPlayer()[0] * 50), (findPlayer()[1] * 50), 50, 50)
+    let playerImg = new Image(50, 50)
+    playerImg.src = './images/big_zombie_idle_anim_f0.png';
+    playerImg.onload = function () {
+        ctx.drawImage(playerImg, (playerCharacter.x), (playerCharacter.y), tileSize, tileSize)
+    }
+}
+function drawMonsters() {
+    for (i = 0; i < monstersInLevel.length; i++) {
+        drawMonster(monstersInLevel[i])
+    }
+}
+
+function drawMonster(monsterID) {
+    let monsterImg = new Image(50, 50)
+    monsterImg.src = './images/big_zombie_idle_anim_f0.png';
+    monsterImg.onload = function () {
+        ctx.drawImage(monsterImg, (monsterID.x), (monsterID.y), tileSize, tileSize)
+    }
+}
+
+function moveMonsters() {
+    for (i = 0; i < monstersInLevel.length; i++) {
+        moveMonster(monstersInLevel[i])
+    }
+}
+
+function moveMonster(monsterID) {
+    let direction = Math.floor(Math.random() * 4 + 37)
+    switch (direction) {
+        case 38: // up arrow
+            if (levelMap[monsterID.x / tileSize][monsterID.y / tileSize - 1].wall == false) {
+                monsterID.moveDirection(0, -(tileSize))
+            }
+            drawBoard()
+            break;
+        case 40: // down arrow
+            if (levelMap[monsterID.x / tileSize][monsterID.y / tileSize + 1].wall == false) {
+                monsterID.moveDirection(0, tileSize)
+            }
+            drawBoard()
+            break;
+        case 37: // left arrow
+            if (levelMap[monsterID.x / tileSize - 1][monsterID.y / tileSize].wall == false) {
+                monsterID.moveDirection(-(tileSize), 0)
+            }
+            drawBoard()
+            break;
+        case 39: // right arrow
+            if (levelMap[monsterID.x / tileSize + 1][monsterID.y / tileSize].wall == false) {
+                monsterID.moveDirection(tileSize, 0)
+            }
+            drawBoard()
+            break;
     }
 }
 
@@ -89,54 +149,72 @@ function drawBoard() {
     clearBoard()
     drawCells()
     drawPlayer()
+    drawMonsters()
+}
+
+function level1() {
+    levelMap[1][1].isPlayerInCell = true
+    levelMap[1][1].wall = false
+    levelMap[1][2].wall = false
+    levelMap[1][3].wall = false
+    levelMap[2][1].wall = false
+    levelMap[2][2].wall = false
+    levelMap[2][3].wall = false
+    levelMap[2][4].wall = false
+    levelMap[2][5].wall = false
+    levelMap[3][5].wall = false
+    levelMap[4][5].wall = false
+    levelMap[4][6].wall = false
+    levelMap[4][7].wall = false
+    levelMap[4][8].wall = false
+    levelMap[5][6].wall = false
+    levelMap[5][7].wall = false
+    levelMap[5][8].wall = false
+
+    levelMap[6][6].wall = false
+    levelMap[6][8].wall = false
+    levelMap[6][7].wall = false
+    levelMap[7][6].wall = false
+    levelMap[7][8].wall = false
+    levelMap[7][7].wall = false
+
+
 }
 
 function startGame() {
+    playerCharacter = new player(50, 50)
+    createMonsters(1)
     populateLevelMapWithCells()
-
-    playerCharacter = new player(levelMap[2][4].x, levelMap[2][4].y)
-    // playerCharacter.x = levelMap[2][4].x
-    // playerCharacter.y = levelMap[2][4].y
+    level1()
     drawBoard()
     document.onkeydown = function (e) {
         switch (e.keyCode) {
             case 38: // up arrow
-                if (levelMap[findPlayer()[0]][(findPlayer()[1] - 1)].border == false) {
-                    let currentX = findPlayer()[0]
-                    let currentY = findPlayer()[1]
-                    levelMap[currentX][currentY - 1].isPlayerInCell = true
-                    levelMap[currentX][currentY].isPlayerInCell = false
+                if (levelMap[playerCharacter.x / tileSize][playerCharacter.y / tileSize - 1].wall == false) {
+                    playerCharacter.moveDirection(0, -(tileSize))
                 }
                 drawBoard()
                 break;
             case 40: // down arrow
-                if (levelMap[findPlayer()[0]][(findPlayer()[1] + 1)].border == false) {
-                    let currentX = findPlayer()[0]
-                    let currentY = findPlayer()[1]
-                    levelMap[currentX][currentY + 1].isPlayerInCell = true
-                    levelMap[currentX][currentY].isPlayerInCell = false
+                if (levelMap[playerCharacter.x / tileSize][playerCharacter.y / tileSize + 1].wall == false) {
+                    playerCharacter.moveDirection(0, tileSize)
                 }
                 drawBoard()
                 break;
             case 37: // left arrow
-                if (levelMap[(findPlayer()[0] - 1)][findPlayer()[1]].border == false) {
-                    let currentX = findPlayer()[0]
-                    let currentY = findPlayer()[1]
-                    levelMap[currentX - 1][currentY].isPlayerInCell = true
-                    levelMap[currentX][currentY].isPlayerInCell = false
+                if (levelMap[playerCharacter.x / tileSize - 1][playerCharacter.y / tileSize].wall == false) {
+                    playerCharacter.moveDirection(-(tileSize), 0)
                 }
                 drawBoard()
                 break;
             case 39: // right arrow
-                if (levelMap[(findPlayer()[0] + 1)][findPlayer()[1]].border == false) {
-                    let currentX = findPlayer()[0]
-                    let currentY = findPlayer()[1]
-                    levelMap[currentX + 1][currentY].isPlayerInCell = true
-                    levelMap[currentX][currentY].isPlayerInCell = false
+                if (levelMap[playerCharacter.x / tileSize + 1][playerCharacter.y / tileSize].wall == false) {
+                    playerCharacter.moveDirection(tileSize, 0)
                 }
                 drawBoard()
                 break;
         }
+        moveMonsters()
     };
 }
 
