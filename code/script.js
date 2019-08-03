ca575485c92dd67b573a6d79dfd11f71e8d5bdaf
Christoph Butenschön <c.butenschoen@gmail.com>
2019-08-03 14:45:43 +0200

    added exit and loot, killing monsters removes them, implemented second level which is initialised upon moving onto exit, implemented first iteration of fog of war function

--- code/script.js
+++ code/script.js
@@ -2,11 +2,25 @@ let gameCanvas
 let ctx
 let playerCharacter
 // mapSize = how many tiles the map square has
-let mapSize = 10;
-let tileSize = 25;
+let mapSize = 40;
+let tileSize = 20;
 let levelMap = []
+let whatLevelAreWeIn = 1
 let monstersInLevel = []
-let state
+let lootTable = []
+let levelExit = {
+    x: 0,
+    y: 0
+}
+let playerInventory = []
+let playerImg = new Image(50, 50)
+playerImg.src = './images/big_zombie_idle_anim_f0.png';
+let monsterImg = new Image(50, 50)
+monsterImg.src = './images/big_demon_idle_anim_f0.png';
+let exitImg = new Image(50, 50)
+exitImg.src = './images/doors_leaf_closed.png';
+let lootImg = new Image(50, 50)
+lootImg.src = './images/chest_empty_open_anim_f0.png';
 
 class cell {
     constructor(x, y) {
@@ -15,13 +29,14 @@ class cell {
         this.heigth = tileSize;
         this.width = tileSize;
         this.wall = true;
+        this.visible = false;
     }
 }
 
 class player {
     constructor(x, y) {
         this.maximumHealth = 50;
-        this.currentHealth = 1;
+        this.currentHealth = 50;
         this.damage = 10;
         this.armor = 4;
         this.experiencePoints = 0;
@@ -53,6 +68,7 @@ class monster {
         this.armor = 1;
         this.x = x;
         this.y = y;
+        this.visible = false;
     }
     moveDirection(horizontal, vertical) {
         this.x += horizontal;
@@ -72,6 +88,56 @@ class monster {
     }
 }
 
+class stationaryObjectsWithLocation {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.visible = false;
+    }
+}
+
+
+class exit extends stationaryObjectsWithLocation {
+    // checkForPlayerProximity(whichObject, numberOfTilesAroundObject)
+    constructor(x, y) {
+        super(x, y)
+    }
+}
+
+
+class weapons {
+    constructor(whatLevelAreWeIn) {
+        this.addedDamage = Math.floor(Math.random() * whatLevelAreWeIn)
+    }
+}
+
+class armor {
+    constructor(whatLevelAreWeIn) {
+        this.addedArmor = Math.floor(Math.random() * whatLevelAreWeIn)
+    }
+}
+
+class loot extends stationaryObjectsWithLocation {
+    // checkForPlayerProximity(whichObject, numberOfTilesAroundObject)
+    constructor(x, y) {
+        super(x, y)
+    }
+
+    giveLoot(whatLevelAreWeIn) {
+        if (Math.floor(Math.random() * 2) == 1) {
+            return new armor(whatLevelAreWeIn)
+        }
+        else {
+            return new weapon(whatLevelAreWeIn)
+        }
+
+    }
+}
+
+
+
+
+
 function populateLevelMapWithCells(level) {
     for (i = 0; i < mapSize; i++) {
         levelMap[i] = new Array()
@@ -81,9 +147,17 @@ function populateLevelMapWithCells(level) {
         }
     }
 }
+
+function populateLootTable(level) {
+    let position = generateAPositionOnMapWithoutWalls()
+    let loot1 = new loot(position[0] * tileSize, position[1] * tileSize)
+    lootTable.push(loot1)
+}
+
 function createMonsters(howMany) {
     for (i = 0; i < howMany; i++) {
-        let monsterVariable = new monster(6 * tileSize, 7 * tileSize)
+        let position = generateAPositionOnMapWithoutWalls()
+        let monsterVariable = new monster(position[0] * tileSize, position[1] * tileSize)
         monstersInLevel.push(monsterVariable)
     }
 }
@@ -94,15 +168,20 @@ function drawCells() {
     for (i = 0; i < levelMap.length; i++) {
         for (j = 0; j < levelMap.length; j++) {
             let location = levelMap[i][j]
-            if (location.wall == true) {
-                ctx.fillStyle = "DarkGrey"
-                ctx.fillRect(location.x, location.y, location.heigth, location.width)
-                ctx.fillStyle = "black"
-            }
-            else {
-                ctx.fillStyle = "grey"
-                ctx.fillRect(location.x, location.y, location.heigth, location.width)
-                ctx.fillStyle = "black"
+            ctx.fillStyle = "black"
+            ctx.fillRect(location.x, location.y, location.heigth, location.width)
+            if (location.visible == true) {
+                if (location.wall == true) {
+                    ctx.fillStyle = "DarkGrey"
+                    ctx.fillRect(location.x, location.y, location.heigth, location.width)
+                    ctx.fillStyle = "black"
+                }
+                else {
+                    ctx.fillStyle = "grey"
+                    ctx.fillRect(location.x, location.y, location.heigth, location.width)
+                    ctx.fillStyle = "black"
+                }
+
             }
         }
     }
@@ -114,27 +193,72 @@ function clearBoard() {
 }
 
 function drawPlayer() {
-    let playerImg = new Image(50, 50)
-    playerImg.src = './images/big_zombie_idle_anim_f0.png';
-    playerImg.onload = function () {
-        ctx.drawImage(playerImg, (playerCharacter.x), (playerCharacter.y), tileSize, tileSize)
-    }
+
+    // playerImg.onload = function () {
+    ctx.drawImage(playerImg, (playerCharacter.x), (playerCharacter.y), tileSize, tileSize)
+    // }
 }
 function drawMonsters() {
-    for (i = 0; i < monstersInLevel.length; i++) {
-        drawMonster(monstersInLevel[i])
+    if (monstersInLevel.length != 0) {
+        for (i = 0; i < monstersInLevel.length; i++) {
+            drawMonster(monstersInLevel[i])
+        }
     }
+}
+
+function generateAPositionOnMapWithoutWalls() {
+    let result = {
+        x: 0,
+        y: 0
+    }
+    let helperArray = []
+    for (i = 0; i < levelMap.length; i++) {
+        for (j = 0; j < levelMap.length; j++) {
+            let location = levelMap[i][j]
+            let posX = i
+            let posY = j
+            console.log
+            if (location.wall == false) {
+                helperArray.push({ x: i, y: j })
+            }
+        }
+    }
+    let randomObjectInArray = Math.floor(Math.random() * helperArray.length)
+    let positionXofRandomPosition = helperArray[randomObjectInArray].x
+    let positionYofRandomPosition = helperArray[randomObjectInArray].y
+    return [positionXofRandomPosition, positionYofRandomPosition]
+
 }
 
 function drawMonster(monsterID) {
-    let monsterImg = new Image(50, 50)
-    monsterImg.src = './images/big_zombie_idle_anim_f0.png';
-    monsterImg.onload = function () {
-        ctx.drawImage(monsterImg, (monsterID.x), (monsterID.y), tileSize, tileSize)
-    }
+
+    // monsterImg.onload = function () {
+    ctx.drawImage(monsterImg, (monsterID.x), (monsterID.y), tileSize, tileSize)
+    // }
 }
 
+function generateLevelExit() {
+    let position = generateAPositionOnMapWithoutWalls()
+    levelExit.x = position[0] * tileSize
+    levelExit.y = position[1] * tileSize
+}
 
+function drawExit() {
+    // exitImg.onload = function () {
+    ctx.drawImage(exitImg, (levelExit.x), (levelExit.y), tileSize, tileSize)
+    // }
+}
+
+function drawLoot() {
+    // if (monstersInLevel.length != 0) {
+    for (i = 0; i < lootTable.length; i++) {
+        let currentLoot = lootTable[i]
+        // lootImg.onload = function () {
+        ctx.drawImage(lootImg, (currentLoot.x), (currentLoot.y), tileSize, tileSize)
+        // }
+    }
+    // }
+}
 
 function moveMonsters() {
     for (i = 0; i < monstersInLevel.length; i++) {
@@ -143,19 +267,29 @@ function moveMonsters() {
     }
 }
 
+function checkIfNearExit() {
+    let x1 = levelExit.x / tileSize
+    let y1 = levelExit.y / tileSize
+    if ((playerCharacter.x / tileSize == x1) && (playerCharacter.y / tileSize == y1)) {
+        whatLevelAreWeIn += 1
+        initLevel()
+    }
+}
+
+// proximity check is called every time the board is redrawn, for example after hero or monster moved, in order to initiate fights
 function checkAllMonstersForProximity() {
     for (i = 0; i < monstersInLevel.length; i++) {
         let currentMonster = monstersInLevel[i]
-        if (checkForPlayerProximity(currentMonster, 1) == true) {
-
+        // checks for playerCharacter.currentHealth to prevent further fights against hero's corpse after hero's deatj
+        if ((checkForPlayerProximity(currentMonster, 1) && (playerCharacter.currentHealth > 0)) == true) {
             fight(currentMonster)
         }
+
     }
 }
 
 function checkForPlayerProximity(whichObject, numberOfTilesAroundObject) {
     //checks whether "whichObject" is in a square centered around "whichObject" which sides are "numberOfTilesAroundObject" away from ""whichObject
-
     // x1 is the x-value of the left border around the whichObject
     let x1 = whichObject.x / tileSize - numberOfTilesAroundObject
     let x2 = whichObject.x / tileSize + numberOfTilesAroundObject
@@ -198,10 +332,16 @@ function moveMonster(monsterID) {
 
 function drawBoard() {
     clearBoard()
+
     drawCells()
-    drawPlayer()
+
+    fogOfWar()
+    drawLoot()
+    drawExit()
+    checkIfNearExit()
     drawMonsters()
     checkAllMonstersForProximity()
+    drawPlayer()
 }
 
 function level1() {
@@ -222,29 +362,127 @@ function level1() {
     levelMap[5][6].wall = false
     levelMap[5][7].wall = false
     levelMap[5][8].wall = false
-
     levelMap[6][6].wall = false
     levelMap[6][8].wall = false
     levelMap[6][7].wall = false
     levelMap[7][6].wall = false
     levelMap[7][8].wall = false
     levelMap[7][7].wall = false
-
-
 }
 
-function init() {
-    playerCharacter = new player(50, 50)
+function level2() {
+    levelMap[3][3].isPlayerInCell = true
+    levelMap[3][3].wall = false
+    levelMap[3][4].wall = false
+    levelMap[3][3].wall = false
+    levelMap[4][3].wall = false
+    levelMap[4][4].wall = false
+    levelMap[4][3].wall = false
+    levelMap[4][4].wall = false
+    levelMap[4][5].wall = false
+    levelMap[3][5].wall = false
+    levelMap[4][5].wall = false
+    levelMap[4][6].wall = false
+    levelMap[4][7].wall = false
+    levelMap[4][8].wall = false
+    levelMap[5][6].wall = false
+    levelMap[5][5].wall = false
+    levelMap[5][8].wall = false
+    levelMap[6][6].wall = false
+    levelMap[6][8].wall = false
+    levelMap[6][5].wall = false
+    levelMap[5][6].wall = false
+    levelMap[5][8].wall = false
+    levelMap[5][5].wall = false
+}
+
+function fogOfWar() {
+    // all cells visible==false
+    for (i = 0; i < levelMap.length; i++) {
+        for (j = 0; j < levelMap.length; j++) {
+            levelMap[i][j].visible == false
+        }
+    }
+    let testXLeft
+    let testYUp
+    let testXRight
+    let testYDown
+    // the visibiliy radius will search over the map array, so we have to exclude cases where it would try to access
+    // tiles outside of the map like levelMap[-1][-2]
+    if (playerCharacter.x / tileSize < 0) {
+        testXLeft = 0
+    } else {
+        testXLeft = playerCharacter.x / tileSize - 2
+    }
+    if (playerCharacter.x / tileSize > mapSize) {
+        testXRight = mapSize
+    } else {
+        testXRight = playerCharacter.x / tileSize + 2
+    }
+    if (playerCharacter.y / tileSize < 0) {
+        testYUp = 0
+    } else {
+        testYUp = playerCharacter.y / tileSize - 2
+    }
+    if (playerCharacter.y / tileSize > mapSize) {
+        testYDown = mapSize
+    } else {
+        testYDown = playerCharacter.y / tileSize + 2
+    }
+    // set visibility == true around the player
+    for (i = (testXLeft); i < (testXRight); i++) {
+        for (j = (testYUp); j < (testYDown); j++) {
+
+            levelMap[i][j].visible = true
+        }
+    }
+
+    // in a radius around the player
+    // if (playerCharacter.x - 2 >= 0) {
+    //     xLeft = playerCharacter.x - 2
+    // }else if (playerCharacter.x - 1 >= 0){
+    // xLeft = playerCharacter.x - 1}
+    // else {
+    //     xLeft = playerCharacter.x
+    // }
+
+
+    // walls
+
+    // but not behind walls
+    // put object.visible == true
+}
+
+function initLevel() {
     monstersInLevel = []
-    // state = unpaused
-    loadInfobar()
-    createMonsters(1)
+    lootTable = []
     populateLevelMapWithCells()
-    level1()
+    if (whatLevelAreWeIn == 1) {
+        level1()
+    }
+    else if (whatLevelAreWeIn == 2) {
+        level2()
+    }
+    else {
+        appendInfoBar("you won! you found the exit out of the dungeon!")
+    }
+    populateLootTable(whatLevelAreWeIn)
+    generateLevelExit()
+    createMonsters(1)
+    if (typeof playerCharacter == "undefined") {
+        let position = generateAPositionOnMapWithoutWalls()
+        playerCharacter = new player(position[0] * tileSize, position[1] * tileSize)
+    } else {
+        let position = generateAPositionOnMapWithoutWalls()
+        playerCharacter.x = position[0] * tileSize
+        playerCharacter.y = position[1] * tileSize
+    }
+    loadInfobar()
 }
+
 
 function startGame() {
-    init()
+    initLevel()
     drawBoard()
     document.onkeydown = function (e) {
         switch (e.keyCode) {
@@ -279,7 +517,7 @@ function startGame() {
 
 function loadInfobar() {
     let topDiv = document.getElementById("game-intro")
-    topDiv.innerHTML = ""
+    topDiv.innerHTML = "" // deletes start button or fight log etc
     let heroStatsDiv = document.createElement('div')
     heroStatsDiv.classList.add("heroStatsDiv")
     heroStatsDiv.innerText = `Health: ${playerCharacter.currentHealth}/${playerCharacter.maximumHealth}
@@ -297,6 +535,7 @@ function updateHeroStatsInInfoBar() {
 
 }
 
+// used to push fight log etc into document
 function appendInfoBar(textForTheDiv) {
     let divToAdd = document.createElement('div')
     divToAdd.innerText = textForTheDiv
@@ -305,7 +544,7 @@ function appendInfoBar(textForTheDiv) {
 
 function appendWinButtonToInfoBar() {
     let buttonToAdd = document.createElement('button')
-    buttonToAdd.innerText = "kk thx"
+    buttonToAdd.innerText = "cool!"
     document.getElementById("game-intro").appendChild(buttonToAdd)
     buttonToAdd.onclick = function () {
         loadInfobar()
@@ -314,15 +553,27 @@ function appendWinButtonToInfoBar() {
 
 function appendLoseButtonToInfoBar() {
     let buttonToAdd = document.createElement('button')
-    buttonToAdd.innerText = "RIP gg start new game"
+    buttonToAdd.innerText = "start new game"
     document.getElementById("game-intro").appendChild(buttonToAdd)
     buttonToAdd.onclick = function () {
         startGame()
     }
 }
 
-function assignLoot(MonsterID) {
+function removeDeadMonstersFromGame(monsterID) {
+    for (i = 0; i < monstersInLevel.length; i++) {
+        if (monstersInLevel[i].currentHealth < 1) {
+            monstersInLevel.splice(i, 1)
+        }
+    }
+}
+
+function getXPAfterFight(MonsterID) {
     return MonsterID.damage * 10
+}
+
+function getLoot(level) {
+
 }
 
 function fight(monsterID) {
@@ -334,10 +585,13 @@ function fight(monsterID) {
         }
     }
     if (playerCharacter.currentHealth > 0) {
-        let XP = assignLoot(monsterID)
+        removeDeadMonstersFromGame(monsterID)
+        let XP = getXPAfterFight(monsterID)
         appendInfoBar(`Hero wins and gets ${XP} XP`)
         playerCharacter.experiencePoints += XP
+        updateHeroStatsInInfoBar()
         appendWinButtonToInfoBar()
+        drawBoard()
     }
     else {
         appendInfoBar(`REKT`)
